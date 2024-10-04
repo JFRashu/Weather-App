584522494068afecb4298f520dbfed2e28f95b35
JFRashu <u2004090@student.cuet.ac.bd>
2024-10-04 21:09:22 +0600

    Navbar And Footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './Routes/Layout'
+import Home from './Routes/Home'
+import Forecast from './Routes/Forecast'
+import Help from './Routes/Help'
+import { About } from './Routes/About'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-     <p>Demo weather Website</p>
-    </>
+    <div className="dark">
+<BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* Use 'index' for the home page route */}
+          <Route index element={<Home />} />  {/* Home route */}
+          <Route path="forecast" element={<Forecast />} />
+          <Route path="help" element={<Help />} />
+          <Route path="about" element={<About />} />
+          {/* Fallback route (for non-existing paths), redirecting to Home */}
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </div>
   )
+
 }
 
 export default App
